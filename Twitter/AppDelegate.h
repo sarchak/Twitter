fc99f6b47ce2dee642d7f5652691ae0a1d52ec8c
Shrikar Archak <shrikar@viralheat.com>
2015-02-19 10:12:56 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Shrikar Archak on 2/19/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Shrikar Archak on 2/19/15.
+//  Copyright (c) 2015 Shrikar Archak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
