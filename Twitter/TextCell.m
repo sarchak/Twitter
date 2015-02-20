6252aa8a00c592d9fe7466870fd4c14e8b39edd0
Shrikar Archak <shrikar@viralheat.com>
2015-02-20 10:06:15 -0800

    Fixed the auto layout issues.

--- Twitter/TextCell.m
+++ Twitter/TextCell.m
@@ -12,7 +12,7 @@
 
 - (void)awakeFromNib {
     // Initialization code
-    self.tweetLabel.preferredMaxLayoutWidth =  self.tweetLabel.frame.size.width;
+//    self.tweetLabel.preferredMaxLayoutWidth =  self.tweetLabel.frame.size.width;
     self.userImageView.layer.cornerRadius = 5.0;
     self.retweetedLabel.hidden = YES;
 }
